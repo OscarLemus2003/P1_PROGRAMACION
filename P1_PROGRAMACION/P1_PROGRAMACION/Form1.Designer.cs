b0a7905f997914a457214ecc688ddd0abcad7587
alumno <alumno@UMG-LAB>
2022-06-05 10:46:20 -0600

    Agregar archivos de proyecto.

---
+++ P1_PROGRAMACION/P1_PROGRAMACION/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace P1_PROGRAMACION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtArtista = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.txtAlbum = new System.Windows.Forms.TextBox();
+            this.txtDisquera = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(726, 195);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAño);
+            this.groupBox1.Controls.Add(this.txtDisquera);
+            this.groupBox1.Controls.Add(this.txtAlbum);
+            this.groupBox1.Controls.Add(this.txtDuracion);
+            this.groupBox1.Controls.Add(this.txtGenero);
+            this.groupBox1.Controls.Add(this.txtArtista);
+            this.groupBox1.Controls.Add(this.txtTitulo);
+            this.groupBox1.Location = new System.Drawing.Point(31, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 235);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(97, 33);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(175, 20);
+            this.txtTitulo.TabIndex = 0;
+            // 
+            // txtArtista
+            // 
+            this.txtArtista.Location = new System.Drawing.Point(97, 74);
+            this.txtArtista.Name = "txtArtista";
+            this.txtArtista.Size = new System.Drawing.Size(175, 20);
+            this.txtArtista.TabIndex = 0;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(97, 115);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(175, 20);
+            this.txtGenero.TabIndex = 0;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Location = new System.Drawing.Point(97, 193);
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(175, 20);
+            this.txtDuracion.TabIndex = 0;
+            // 
+            // txtAlbum
+            // 
+            this.txtAlbum.Location = new System.Drawing.Point(97, 158);
+            this.txtAlbum.Name = "txtAlbum";
+            this.txtAlbum.Size = new System.Drawing.Size(175, 20);
+            this.txtAlbum.TabIndex = 0;
+            // 
+            // txtDisquera
+            // 
+            this.txtDisquera.Location = new System.Drawing.Point(427, 36);
+            this.txtDisquera.Name = "txtDisquera";
+            this.txtDisquera.Size = new System.Drawing.Size(175, 20);
+            this.txtDisquera.TabIndex = 0;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(427, 74);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(175, 20);
+            this.txtAño.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TItulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Genero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Album";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Duracion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(263, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 28);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "REGISTRO DE MUSICA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Artista";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(347, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Año";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(347, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Disquera";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 21);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Nuevo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(528, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 21);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Eliminar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 525);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtDisquera;
+        private System.Windows.Forms.TextBox txtAlbum;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtArtista;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ P1_PROGRAMACION/P1_PROGRAMACION/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace P1_PROGRAMACION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            MessageBox.Show("Conexion exitosa compa");
+
+            dataGridView1.DataSource = llenar_grid();
+        }
+        public DataTable llenar_grid()
+        {
+            Conexion.Conectar();
+            DataTable dt = new DataTable();
+            string consulta = "select * from tb_Musica";
+            SqlCommand cmd = new SqlCommand(consulta,Conexion.Conectar());  
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string insertar = "INSERT INTO tb_Musica (Titulo,Artista,Genero,Album,Duracion,Disquera,Año)values(@Titulo,@Artista,@Genero,@Album,@Duracion,@Disquera,@Año)";
+            SqlCommand cmd1 = new SqlCommand(insertar,Conexion.Conectar());
+            cmd1.Parameters.AddWithValue("@Titulo",txtTitulo.Text);
+            cmd1.Parameters.AddWithValue("@Artista",txtArtista.Text);
+            cmd1.Parameters.AddWithValue("@Genero",txtGenero.Text);
+            cmd1.Parameters.AddWithValue("@Album",txtAlbum.Text);
+            cmd1.Parameters.AddWithValue("@Duracion",txtDuracion.Text);
+            cmd1.Parameters.AddWithValue("@Disquera",txtDisquera.Text);
+            cmd1.Parameters.AddWithValue("@Año",txtAño.Text);
+
+            cmd1.ExecuteNonQuery();
+            MessageBox.Show("Los datos fueron agreagados correctamente que proooo");
+
+            dataGridView1.DataSource = llenar_grid();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try {
+
+                txtTitulo.Text=dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtArtista.Text=dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtGenero.Text=dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                txtAlbum.Text=dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                txtDuracion.Text=dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                txtDisquera.Text=dataGridView1.CurrentRow.Cells[5].Value.ToString();
+                txtAño.Text=dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            
+            }
+
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string actualizar = "UPDATE tb_Musica SET Titulo=@Titulo,Artista=@Artista,Genero=@Genero,Album=@Album,Duracion=@Duracion,Disquera=@Disquera,Año=@Año where Titulo=@Titulo";
+            SqlCommand cmd2 = new SqlCommand(actualizar, Conexion.Conectar());
+
+            cmd2.Parameters.AddWithValue("@Titulo",txtTitulo.Text);
+            cmd2.Parameters.AddWithValue("@Artista",txtArtista.Text);
+            cmd2.Parameters.AddWithValue("@Genero",txtGenero.Text);
+            cmd2.Parameters.AddWithValue("@Album",txtAlbum.Text);
+            cmd2.Parameters.AddWithValue("@Duracion",txtDuracion.Text);
+            cmd2.Parameters.AddWithValue("@Disquera",txtDisquera.Text);
+            cmd2.Parameters.AddWithValue("@Año",txtAño.Text);
+
+            cmd2.ExecuteNonQuery();
+            MessageBox.Show("Los datos han sido actualizados bro");
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string eliminar = "DELETE FROM tb_Musica where Titulo=@Titulo";
+            SqlCommand cmd3 = new SqlCommand(eliminar, Conexion.Conectar());
+            cmd3.Parameters.AddWithValue("@Titulo", txtTitulo.Text);
+
+            cmd3.ExecuteNonQuery();
+            MessageBox.Show("Los datos fueron borrados del mapa mi hermano");
+
+            dataGridView1.DataSource = llenar_grid();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtTitulo.Clear();
+            txtArtista.Clear();
+            txtGenero.Clear();
+            txtAlbum.Clear();   
+            txtDuracion.Clear();
+            txtDisquera.Clear();
+            txtAño.Clear();
+            txtTitulo.Focus();
+           
+         
+        }
+    }
+}
